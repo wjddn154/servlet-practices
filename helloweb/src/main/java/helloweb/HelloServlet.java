27aa7c4faedda84d7dc47b3df3f8001554d182a4
wjddn154 <gjwjddn154@gmail.com>
2021-10-07 17:02:46 +0900

    servlet-practices 처음 실습부분

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,29 @@
+
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;import org.omg.CORBA.Request;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter pw = response.getWriter();
+		pw.print("<h1>Hello World</h1>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+
+
+}
