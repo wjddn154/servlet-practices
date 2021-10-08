014906c516677e13f809593d0f81b0d9e00ae0da
wjddn154 <gjwjddn154@gmail.com>
2021-10-08 17:05:26 +0900

    guestbook02 #1

---
+++ docs/guestbook.sql
@@ -0,0 +1,16 @@
+desc guestbook;
+select * from guestbook;
+
+-- insert
+insert into guestbook values(null, '둘리', '1234', '호이욤', now());
+
+-- select
+  select no, name, date_format(reg_date, '%Y/%m/%d %H:%i:%s'), message
+    from guestbook
+order by reg_date desc;
+
+-- delete
+delete
+  from guestbook
+ where no=1
+   and password='1234';
